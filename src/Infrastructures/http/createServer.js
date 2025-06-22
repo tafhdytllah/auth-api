8ad82728cae0bf036c35362b3c3cdcd818a1ce8d
Taufik Hidayatullah <taufik@fungsitama.com>
2025-06-22 19:42:00 +0700

    change test config

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -2,12 +2,14 @@ const Hapi = require('@hapi/hapi');
 const ClientError = require('../../Commons/exceptions/ClientError');
 const DomainErrorTranslator = require('../../Commons/exceptions/DomainErrorTranslator');
 const users = require('../../Interfaces/http/api/users');
+const config = require('../../Commons/config');
 const authentications = require('../../Interfaces/http/api/authentications');
 
 const createServer = async (container) => {
   const server = Hapi.server({
-    host: process.env.HOST,
-    port: process.env.PORT,
+    port: config.app.port,
+    host: config.app.host,
+    debug: config.app.debug,
   });
 
   await server.register([
